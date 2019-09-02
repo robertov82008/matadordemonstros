bf55904061db11e06b3762bb9cd391fb75a4daf8
Roberto Ribeiro <robertov82008@gmail.com>
2019-09-02 17:45:08 -0300

    66. Adicionando o Registro de Log #02

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ new Vue({
             this.running = true;
             this.playerLife = 100;
             this.monsterLife = 100;
+            this.logs = [];
         },
         giveUp() {
             this.running = false;
@@ -34,11 +35,12 @@ new Vue({
         },
         healAndHurt() {
             this.heal(10, 15);
-            this.hurt('playerLife', 7, 12, false);
+            this.hurt('playerLife', 7, 12, false, 'Monstro', 'Jogador', 'monster');
         },
         heal(min, max) {
             const heal = this.getRandom(min, max);
             this.playerLife = Math.min(this.playerLife + heal, 100);
+            this.registerLog(`Jogador ganhou força de ${heal}.`, 'player');
         },
         getRandom(min, max) {
             const value = Math.random() * (max - min) + min;
