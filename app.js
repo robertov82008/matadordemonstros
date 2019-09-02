bd07e97e9b7c1addca1e1c5625967e5a80ba7d7e
Roberto Ribeiro <robertov82008@gmail.com>
2019-09-02 16:17:14 -0300

    62. Iniciar Jogo & Desistir do Jogo

--- app.js
+++ app.js
@@ -11,7 +11,14 @@ new Vue({
         }
     },
     methods: {
-    
+        startGame() {
+            this.running = true;
+            this.playerLife = 100;
+            this.monsterLife = 100;
+        },
+        giveUp() {
+            this.running = false;
+        }
     },
     watch: {
     
